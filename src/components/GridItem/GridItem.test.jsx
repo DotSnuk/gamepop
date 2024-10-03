5cde1ad7695c415401bfc48b0dfcaf110dc81678
Hampus Farner <hampusfarner@outlook.com>
2024-10-03 12:14:49 +0200

    Test GridItem for name and image

--- src/components/GridItem/GridItem.test.jsx
+++ src/components/GridItem/GridItem.test.jsx
@@ -4,6 +4,7 @@ import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 import { routes } from '../../app/routes';
 import userEvent from '@testing-library/user-event';
 import GridItem from './GridItem';
+import img from '../../assets/placeholder_300x300.png';
 
 it('display game name', () => {
   const game = { name: 'GTA' };
@@ -11,4 +12,15 @@ it('display game name', () => {
   render(<GridItem game={game} />);
 
   expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent(/gta/i);
+});
+
+it('shows image', () => {
+  const game = { name: 'GTA', background_image: img };
+
+  render(<GridItem game={game} />);
+
+  expect(screen.getByRole('img')).toHaveAttribute(
+    'src',
+    expect.stringMatching(/png$/),
+  );
 });
